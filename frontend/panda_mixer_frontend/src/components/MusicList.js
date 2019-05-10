f627064e5ec2ecb20d85e3aad76ef2089ca45d5c
jakubdybczak <jakub.dybczak@gmail.com>
2019-05-11 01:58:11 +0200

    Add delete functionality

--- frontend/panda_mixer_frontend/src/components/MusicList.js
+++ frontend/panda_mixer_frontend/src/components/MusicList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import { List } from 'antd';
+import { List, Button } from 'antd';
 import './MusicList.css';
 
 export default class MusicList extends Component {
@@ -51,6 +51,24 @@ export default class MusicList extends Component {
         clearInterval(this.interval);
     }
 
+    handleDeleteClick = (item) => {
+        fetch('http://127.0.0.1:8000/api/playlists/' + this.props.playlistId + "/elements/" + item.order + "/", {
+            method: 'delete',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+        }).then(function (response) {
+            return response.json();
+        }
+        )
+            .then((data) => {
+
+            })
+            .catch((err) => {
+                console.log(err)
+            }
+            )
+    }
     render() {
 
 
@@ -65,7 +83,11 @@ export default class MusicList extends Component {
                             <List.Item>
                                 <div style={{ width: "100%" }}>
                                     <p className="Left-Item">{item.data}</p>
-                                    <p className="Right-Item" >Siema</p>
+                                    <p className="Right-Item" >
+                                        <Button style={{ width: "5em" }} type="primary" htmlType="submit" onClick={() => this.handleDeleteClick(item)}>
+                                            Delete
+                                         </Button>
+                                    </p>
                                 </div>
                             </List.Item>
                         )}
