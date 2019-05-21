7031c0789ad3eb91667ad17d3c90c50b3a89490f
jakubdybczak <jakub.dybczak@gmail.com>
2019-05-21 21:16:33 +0200

    Clean backend

--- backend/mysite/wedj/urls.py
+++ backend/mysite/wedj/urls.py
@@ -6,22 +6,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 router = routers.DefaultRouter()
-#router.register(r'playlist', views.PlaylistViewSet)
-#router.register(r'playlist2', views.playlist_view)
-
 
 urlpatterns = [
-    #path('api/', views.PlaylistViewSet.as_view({'get': 'list'})),
-    #path('api/', views.PlaylistView.as_view()),
-    #path('api/', include(router.urls)),
     path('api/playlists/', views.PlaylistsView.as_view()),
     path('api/playlists/<str:link_id>/', views.PlaylistDetailsView.as_view()),
     path('api/playlists/<str:link_id>/elements/', views.PlaylistElementsView.as_view()),
     path('api/playlists/<str:link_id>/elements/<int:order>/', views.PlaylistElementDetailView.as_view()),
     path('api/users/', views.UsersView.as_view()),
-    path('api/users/<str:user>', views.UserDetailsView.as_view()),
-    path('', views.index, name='index'),
-    path('p/<str:playlist_id>/', views.playlist, name='playlist'),
+    path('api/users/<str:user>/', views.UserDetailsView.as_view()),
     path('api/ytquery/<str:query>/', views.youtube_query),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/refresh_token/', TokenRefreshView.as_view()),
